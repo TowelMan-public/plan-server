c81949713c18af7b8739bfd901a12916ded44702
TowelMan-public <OdagiriTomoya@outlook.jp>
2021-10-29 11:20:21 +0900

    UserConfigFormの修正

--- src/main/java/com/example/demo/form/UserConfigForm.java
+++ src/main/java/com/example/demo/form/UserConfigForm.java
@@ -1,9 +1,13 @@
 package com.example.demo.form;
 
+import javax.validation.constraints.AssertTrue;
+
 import lombok.Getter;
 import lombok.Setter;
 
 public class UserConfigForm {
+	private static final Integer MIN_DEADLINE = 86400;
+	
 	@Getter
 	@Setter
 	private Integer beforeDeadlineForTodoNotice;
@@ -19,4 +23,16 @@ public class UserConfigForm {
 	@Getter
 	@Setter
 	private Boolean isPushSatrtedTodoNotice;
+	
+	@AssertTrue(message = "beforeDeadlineForTodoNotice must be bigger than 86400 or as big as 86400.")
+	public boolean isBeforeDeadlineForTodoNoticeAppropriate() {
+		return beforeDeadlineForTodoNotice == null ||
+				beforeDeadlineForTodoNotice >= MIN_DEADLINE;
+	}
+	
+	@AssertTrue(message = "beforeDeadlineForProjectNotice must be bigger than 86400 or as big as 86400.")
+	public boolean isBeforeDeadlineForProjectNoticeAppropriate() {
+		return beforeDeadlineForProjectNotice == null ||
+				beforeDeadlineForProjectNotice >= MIN_DEADLINE;
+	}
 }
