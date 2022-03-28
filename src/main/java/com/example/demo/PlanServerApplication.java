026d6f67eec3e7a660742e47fc2ce6aa1a7abba1
TowelMan-public <OdagiriTomoya@outlook.jp>
2022-03-28 11:26:04 +0900

    test10

--- src/main/java/com/example/demo/PlanServerApplication.java
+++ src/main/java/com/example/demo/PlanServerApplication.java
@@ -14,7 +14,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class PlanServerApplication {
 
 	public static void main(String[] args) {
-		//run
 		SpringApplication.run(PlanServerApplication.class, args);
 	}
 
